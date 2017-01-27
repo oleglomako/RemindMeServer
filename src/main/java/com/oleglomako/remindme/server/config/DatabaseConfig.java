ce5acfa813dc07b2182695d89c14ebdcc2da0191
oleglomako <lom-o@yandex.ru>
2017-01-27 18:29:54 +0100

    DatabaseConfig edit

--- src/main/java/com/oleglomako/remindme/server/config/DatabaseConfig.java
+++ src/main/java/com/oleglomako/remindme/server/config/DatabaseConfig.java
@@ -1,7 +1,13 @@
 package com.oleglomako.remindme.server.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
 
 /**
  * Created by oleg on 27.01.17.
@@ -14,7 +20,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // говорим сприг-котексту что будем использовать JPA а именно спринг-дата
 // который построен на паттерне репозиторий
-@EnableJpaRepositories()
+@EnableJpaRepositories("com.oleglomako.remindme.server.repository")
+
+// включаем поддержку транзакций
+@EnableTransactionManagement
+
+// говорим где искать все репозитории и конфиги
+@ComponentScan("com.oleglomako.remindme.server")
 
 public class DatabaseConfig {
+
+    // с его помощю получим доступ к нашим проперти файлам
+    // и сможем получать из проперти файла какие то значения
+    @Resource
+    private Environment env;
+
 }
