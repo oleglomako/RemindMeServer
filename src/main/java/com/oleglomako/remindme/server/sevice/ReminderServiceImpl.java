1cebda01e93bf39e4da0bbce8b19b7260bfe2263
oleglomako <lom-o@yandex.ru>
2017-01-29 23:29:03 +0100

    ReminderServiceImpl added

--- src/main/java/com/oleglomako/remindme/server/sevice/ReminderServiceImpl.java
+++ src/main/java/com/oleglomako/remindme/server/sevice/ReminderServiceImpl.java
@@ -1,7 +1,45 @@
 package com.oleglomako.remindme.server.sevice;
 
+import com.oleglomako.remindme.server.entity.Remind;
+import com.oleglomako.remindme.server.repository.RemindRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created by oleg on 29.01.17.
  */
-public class ReminderServiceImpl {
+
+@Service
+public class ReminderServiceImpl implements ReminderService{
+
+    // данные сервисы могут выполнять разную логику
+    // TODO тут можно подключить наши репозитории их может быть несколько -> @
+    // TODO также тут могут быть джава-мейл сервисы и т п
+    // TODO тут можно дергать другие внешние системы рест апи
+    // TODO здесь выполняем всю бизнеслогику а контродллеры их просто вызывают
+
+
+    // но мы пока будем просто вызывать наш репозиторий так как унас пока нет больше логики
+    // проинициализируем наш репозиторий
+    @Autowired
+    private RemindRepository repository;
+
+
+    public List<Remind> getAll() {
+        return repository.findAll();
+    }
+
+    public Remind getByID(long id) {
+        return repository.findOne(id);
+    }
+
+    public Remind save(Remind remind) {
+        return repository.saveAndFlush(remind);
+    }
+
+    public void remove(long id) {
+        repository.delete(id);
+    }
 }
