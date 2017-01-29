d7bfb9f5aeed24a37a8be3ed1665c94c9865b71d
oleglomako <lom-o@yandex.ru>
2017-01-29 12:26:53 +0100

    added dependecy jackson for cconvert obj to JSON

--- src/main/java/com/oleglomako/remindme/server/config/WebConfig.java
+++ src/main/java/com/oleglomako/remindme/server/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.oleglomako.remindme.server.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -27,6 +28,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        super.configureMessageConverters(converters);
+        // будем использовать Jackson для конвертации в JSON
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper();
     }
 }
