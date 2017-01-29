039ba313ccce2c7c24a9bbf4187dbfd201920da6
oleglomako <lom-o@yandex.ru>
2017-01-29 12:45:14 +0100

    interface RemindRepository added

---
+++ src/main/java/com/oleglomako/remindme/server/repository/RemindRepository.java
@@ -0,0 +1,14 @@
+package com.oleglomako.remindme.server.repository;
+
+import com.oleglomako.remindme.server.entity.Remind;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by oleg on 29.01.17.
+ *
+ * репозиторий для сущности Remind
+ */
+
+public interface RemindRepository extends JpaRepository<Remind,Long> {
+
+}
